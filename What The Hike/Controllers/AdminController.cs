87c30cb367e64a577cdbf97abd3137991f576c93
Kiara Smith <45629725+kiaracsmith@users.noreply.github.com>
2021-04-15 20:08:12 +0200

    Added admin controller

---
+++ What The Hike/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace What_The_Hike.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public String getLocation()
+        {
+
+        }
+
+        [HttpPost]
+        public String LoadLocation()
+        {
+
+        }
+
+        [HttpPatch]
+        public String updateLocation()
+        {
+
+        }
+
+        [HttpDelete]
+        public String removeLocation()
+        {
+
+        }
+    }
+}
